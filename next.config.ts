2d15ffbcead88debb518598282a80ffef9d51c5b
Saurabh Kumar <saukumar95@gmail.com>
2024-12-22 20:05:25 +0530

    fixed env key issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ const nextConfig: import('next').NextConfig = {
     output: 'export',
     basePath: '/weather-app-next-js',
     reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_WEATHER_KEY: process.env.NEXT_PUBLIC_WEATHER_KEY,
+    },
     images: {
         unoptimized: true,
         remotePatterns: [
