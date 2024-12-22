88d58167be678d145341f12bb724876215424b75
Saurabh Kumar <saukumar95@gmail.com>
2024-12-22 21:59:08 +0530

    fix current location name displayed in nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ export default function Home() {
 
     return (
         <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
-            <Navbar />
+            <Navbar location={data?.city.name} />
             <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9  w-full  pb-10 pt-4">
                 {/* Current Weather */}
                 {loadingCity ? (
